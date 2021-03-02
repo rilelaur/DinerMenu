cea041fa598d0b569cb1c0e2a887036316e8e210
Laura <laurakriley01@gmail.com>
2021-03-02 08:39:00 -0700

    Add project files.

---
+++ DinerMenuForm.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DinerMenuForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DisplaySpecialLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Font = New System.Drawing.Font("MV Boli", 18.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DinerNameLabel.Location = New System.Drawing.Point(284, 57)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(216, 40)
+        Me.DinerNameLabel.TabIndex = 0
+        Me.DinerNameLabel.Text = "Laura's Diner"
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Location = New System.Drawing.Point(20, 364)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(133, 65)
+        Me.SoupButton.TabIndex = 1
+        Me.SoupButton.Text = "Soup"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Location = New System.Drawing.Point(327, 362)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(133, 65)
+        Me.FishButton.TabIndex = 3
+        Me.FishButton.Text = "Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(172, 362)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(133, 65)
+        Me.SaladButton.TabIndex = 2
+        Me.SaladButton.Text = "Salad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(648, 362)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(133, 65)
+        Me.ExitButton.TabIndex = 4
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DisplaySpecialLabel
+        '
+        Me.DisplaySpecialLabel.Font = New System.Drawing.Font("MV Boli", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(167, 97)
+        Me.DisplaySpecialLabel.Name = "DisplaySpecialLabel"
+        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(455, 221)
+        Me.DisplaySpecialLabel.TabIndex = 6
+        Me.DisplaySpecialLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'DinerMenuForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DisplaySpecialLabel)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.Name = "DinerMenuForm"
+        Me.Text = "Laura's Diner"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DisplaySpecialLabel As Label
+End Class
---
+++ DinerMenuForm.vb
@@ -0,0 +1,34 @@
+﻿'Laura Riley
+'RCET0265
+'Spring 2021
+'Diner Menu Program
+'
+
+Public Class DinerMenuForm
+
+    'Displays the soup of the day
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+        DisplaySpecialLabel.Text = "~Soup of the day~ " & vbCrLf & "Bacon Potoato Soup"
+    End Sub
+
+    'Displays the chefs special salad
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+        DisplaySpecialLabel.Text = "~The Chefs Special~" & vbCrLf &
+            "The salad is made from a special organic array of grasses and leaves" _
+            & " with fresh berries added and topped with a crisp vinigret." _
+            & "Your taste buds will thank you for generations to come."
+    End Sub
+
+    'Displays the fish of the day
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+        DisplaySpecialLabel.Text = "~Fresh Found Fish~" & vbCrLf _
+           & "Fish found in the area today is the Bluehead Sucker native to the Protneuf River" & vbCrLf _
+           & "*Note: we are not responsible for any ill side effects of ingesting anything that " _
+           & "comes from the Portneuf River."
+    End Sub
+
+    'Exits the program
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+End Class
